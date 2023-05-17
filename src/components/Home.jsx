f48664ce85ceb889c5bc94acebb2c115fea266b4
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-17 10:29:34 +0600

    Home Front end

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container my-3">
+      <div className="row ">
+        <div className="col-6 col-md-3 mb-2">
+          <div className="card p-2">
+            <small>1</small>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nihil
+              quam tempora, error iusto fugiat.
+            </p>
+            <h5>Lorem ipsum dolor sit amet.</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
